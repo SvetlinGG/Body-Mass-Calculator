8f79f7a82de80565a9898444bd9f207c78ffe786
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-07 22:45:02 +0100

    styling header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,9 +7,9 @@ export default function Header(){
             <div><img className="logo" src="/logo.svg" alt="logo" /></div>
             <div className="header-items">
                 <h1>Body Mass<br/> Index Calculator</h1>
-                <p> Better understand your weight in relation to your height using our 
-                    body mass index (BM) calculator. While BMI is not the sole determinant 
-                    of a healthy weight, it offers a valuable starting point to evaluate 
+                <p> Better understand your weight in relation to your height using<br/> our 
+                    body mass index (BM) calculator. While BMI is not the<br/> sole determinant 
+                    of a healthy weight, it offers a valuable<br/> starting point to evaluate 
                     your overall health and well-being.</p>
             </div>
         </div>
