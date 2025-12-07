9fd41dac82c2d85d801fdc778278fdbb6fa25378
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-12-07 23:43:59 +0100

    init main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Header from './Components/Header'
+import Main from './Components/Main'
 
 function App() {
  
 
   return (
-    
+    <>
     <Header />
-    
+
+    <Main />
+    </>
   )
 }
 
