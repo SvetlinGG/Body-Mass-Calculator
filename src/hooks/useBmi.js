012109b541f4fcefa466703274266fca6cfac059
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-02 00:45:46 +0200

    setup use bmi metric

---
+++ src/hooks/useBmi.js
@@ -0,0 +1,40 @@
+import { useMemo, useState } from "react";
+import { healthyWeightRangeMetrics, classifyBmi, calcBmiFromMetric, calcBmiFromImperial,  } from "../utils/bmi";
+
+
+export function useBmi(){
+    const [unit, setUnit] = useState('metric');
+
+    const [metric, setMetric] = useState({heightCm: '', weightKg: ''});
+    const [imperial, setImperial] = useState({
+        heightFt: '',
+        heightIn: '',
+        weightSt: '',
+        weightLb: '',
+    });
+
+    const bmi = useMemo(() => {
+        return unit === 'metric'
+        ? calcBmiFromMetric(metric)
+        : calcBmiFromImperial(imperial)
+    }, [unit, metric, imperial])
+
+    const classification = useMemo(() => classifyBmi(bmi), [bmi]);
+
+    const healthyRange = useMemo(() => {
+        if (unit !== 'metric') return null;
+        return healthyWeightRangeMetrics(metric.heightCm)
+    }, [unit, metric.heightCm]);
+
+    return {
+        unit,
+        setUnit,
+        metric,
+        setMetric,
+        imperial,
+        setImperial,
+        bmi,
+        classification,
+        healthyRange,
+    };
+}
