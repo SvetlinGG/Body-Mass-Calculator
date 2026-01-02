fddc358af4b885291e4d9ac7f4f32fb82acaee8e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-03 00:38:33 +0200

    app component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App.jsx';
+import "./styles/tokens.css";
+import "./styles/globals.css";
 
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode >
     <App />
+  </React.StrictMode>
+    
   
 )
